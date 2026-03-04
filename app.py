b36369b73d119e657a9999677cd9639fe84affb0
salih rıdvan yılmaz <185217082+sridvany@users.noreply.github.com>
2026-03-04 14:34:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -146,10 +146,11 @@ else:
     * **Döviz / Emtia:** `EURUSD=X`, `GC=F` (Altın), `CL=F` (Petrol)
 
     ---
-    *Uygulama yatırım tavsiyesi içermez. Tradingview gibi sitelerde aylık 50-100 dolar arası maliyeti olan bir analizi ücretsiz olarak sunar.*
+    *Uygulama yatırım tavsiyesi içermez. Aylık 50-100 dolar arası maliyeti olan sitelerdeki bir analizi ücretsiz olarak sunar.*
     ---
     *salih rıdvan yılmaz*
     """)
+
 
 
 
