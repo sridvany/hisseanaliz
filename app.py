6618736dd2a408bdc062b30d90b38e60271f6739
salih rıdvan yılmaz <185217082+sridvany@users.noreply.github.com>
2026-03-08 14:02:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -358,7 +358,7 @@ def create_complete_trading_chart(ticker, start, end, per, k_len, s_mult, srsi_l
             x=[df.index[0]], y=[df['Close'].iloc[0]], 
             mode='lines',
             line=dict(color='rgba(0,0,0,0)', width=0), # Çizgi tamamen görünmez
-            name='<span style="font-size:12px; color:red; font-weight:bold;">Üstüne tıklayarak<br>indikatörü açabilirsiniz</span>',
+            name='<span style="font-size:12px; color:red; font-weight:bold;"> Üstüne tıklayarak<br>indikatörü açabilirsiniz</span>',
             showlegend=True,
             hoverinfo='skip' # Mouse üzerine gelince etkileşim olmasın
         ), row=1, col=1
@@ -546,5 +546,6 @@ else:
 
     *(Salih Rıdvan Yılmaz - sry@tahmin.ai)*
     """)
+
 
 
